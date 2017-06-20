885995ae492e20e2d2d333edf2d5677ecad0809f
Derek DeJonghe <derek.dejonghe@rightbrainnetworks.com>
2017-06-19 22:35:36 -0400

    update build script to not clutter

--- scripts/build_lambdas.sh
+++ scripts/build_lambdas.sh
@@ -1,7 +1,21 @@
 #!/bin/bash 
 
-#build attach hosted zone
-cd lambda/cats/
+temp_path=lambda/.temp/
+
+# Make a temp dir to build in 
+mkdir $temp_path
+
+# Copy code to the temp path
+cp lambda/cats/* $temp_path
+
+# Install requirements to temp path
+cd $temp_path
 pip install -r requirements.txt -t .
+
+# Make a build directory and zip up the build package
 mkdir -p ../../builds
 zip -r ../../builds/cats.zip ./*
+
+# Remove the temparary build dir
+cd ../../
+rm -r $temp_path
